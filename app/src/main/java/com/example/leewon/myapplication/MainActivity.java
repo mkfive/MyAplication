682133488240d85b494f1d97832abdc4b05dbb62
leewon <mkfive@naver.com>
2016-01-04 16:37:35 +0900

    123456

--- app/src/main/java/com/example/leewon/myapplication/MainActivity.java
+++ app/src/main/java/com/example/leewon/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//leewonstefefㄷㄹㄴㄹㄴ강낭콩
+
 public class MainActivity extends ActionBarActivity {
 
     @Override
